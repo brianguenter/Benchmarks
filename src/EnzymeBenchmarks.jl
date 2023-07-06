43d3df13bf20079f360029cd542722abc4976308
brianguenter <1brianguenter@gmail.com>
2023-07-05 19:01:42 -0700

    added incorrect enzyme benchmark in the hopes in can be fixed.

--- src/EnzymeBenchmarks.jl
+++ src/EnzymeBenchmarks.jl
@@ -103,3 +103,91 @@ function enzyme_ODE()
     @benchmark Enzyme.jacobian(Enzyme.Forward, ODE.enzyme_f, $y, Val($nterms))
 end
 export enzyme_ODE
+
+# #does not work
+# k1 = .35e0
+# k2 = .266e2
+# k3 = .123e5
+# k4 = .86e-3
+# k5 = .82e-3
+# k6 = .15e5
+# k7 = .13e-3
+# k8 = .24e5
+# k9 = .165e5
+# k10 = .9e4
+# k11 = .22e-1
+# k12 = .12e5
+# k13 = .188e1
+# k14 = .163e5
+# k15 = .48e7
+# k16 = .35e-3
+# k17 = .175e-1
+# k18 = .1e9
+# k19 = .444e12
+# k20 = .124e4
+# k21 = .21e1
+# k22 = .578e1
+# k23 = .474e-1
+# k24 = .178e4
+# k25 = .312e1
+
+# function tuple_f(y)
+#     r1 = k1 * y[1]
+#     r2 = k2 * y[2] * y[4]
+#     r3 = k3 * y[5] * y[2]
+#     r4 = k4 * y[7]
+#     r5 = k5 * y[7]
+#     r6 = k6 * y[7] * y[6]
+#     r7 = k7 * y[9]
+#     r8 = k8 * y[9] * y[6]
+#     r9 = k9 * y[11] * y[2]
+#     r10 = k10 * y[11] * y[1]
+#     r11 = k11 * y[13]
+#     r12 = k12 * y[10] * y[2]
+#     r13 = k13 * y[14]
+#     r14 = k14 * y[1] * y[6]
+#     r15 = k15 * y[3]
+#     r16 = k16 * y[4]
+#     r17 = k17 * y[4]
+#     r18 = k18 * y[16]
+#     r19 = k19 * y[16]
+#     r20 = k20 * y[17] * y[6]
+#     r21 = k21 * y[19]
+#     r22 = k22 * y[19]
+#     r23 = k23 * y[1] * y[4]
+#     r24 = k24 * y[19] * y[1]
+#     r25 = k25 * y[20]
+
+#     return (
+#         -r1 - r10 - r14 - r23 - r24 +
+#         r2 + r3 + r9 + r11 + r12 + r22 + r25, -r2 - r3 - r9 - r12 + r1 + r21, -r15 + r1 + r17 + r19 + r22, -r2 - r16 - r17 - r23 + r15, -r3 + r4 + r4 + r6 + r7 + r13 + r20, -r6 - r8 - r14 - r20 + r3 + r18 + r18, -r4 - r5 - r6 + r13, r4 + r5 + r6 + r7, -r7 - r8, -r12 + r7 + r9, -r9 - r10 + r8 + r11, r9, -r11 + r10, -r13 + r12, r14, -r18 - r19 + r16, -r20, r20, -r21 - r22 - r24 + r23 + r25, -r25 + r24
+#     )
+# end
+
+# float_y = rand(20)
+# # float_dy = rand(20)
+# # d_dy = Enzyme.BatchDuplicatedNoNeed(ones(20), tuple((zeros(length(float_y)) for i in 1:20)...))
+# # td_y = Enzyme.BatchDuplicated(tuple(float_y...), Enzyme.onehot(tuple(float_y...)))
+# tfloat_y = tuple(float_y...)
+
+# makeOneHot() = Enzyme.onehot(NTuple{20,Float64})
+
+
+# function jac(y)
+#     mode = Enzyme.Forward
+#     func = tuple_f
+#     Enzyme.autodiff(mode, Enzyme.Const(func), Enzyme.DuplicatedNoNeed, Enzyme.BatchDuplicatedFunc{typeof(y),20,typeof(makeOneHot)}(y))
+# end
+
+
+
+# # @benchmark $jac($tfloat_y)
+# jac(tfloat_y)
+# # nterms = 20
+# # y = rand(nterms)
+# # @benchmark Enzyme.jacobian(Enzyme.Forward, ODE.enzyme_f, $y, Val($nterms))
+# # J = similar(float_y, 20, 20)
+# # ODE.fjac(J, float_y, nothing, nothing)
+# # # @assert jac(tfloat_y) ≈ J
+# # println(jac(tfloat_y))
+# # println(J)
